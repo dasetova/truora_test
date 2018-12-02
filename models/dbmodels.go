f24e7391fd97ffb1fcb7227bf757653cfd099dc8
Daniel Torres <dasetova@gmail.com>
2018-12-02 15:06:42 -0500

    Work in progress, helpers to database operations. Pendings RUD operations for recipes

---
+++ models/dbmodels.go
@@ -0,0 +1,26 @@
+package models
+
+type Category struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Recipe struct {
+	ID          int      `json:"id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Indications string   `json:"indications"`
+	CategoryID  int      `json:"category_id"`
+	Category    Category `json:"category"`
+
+	Ingredients []Ingredient `json:"ingredients"`
+}
+
+type Ingredient struct {
+	ID          int     `json:"id"`
+	Description string  `json:"description"`
+	Quantity    float64 `json:"quantity"`
+	MeasureUnit string  `json:"measure_unit"`
+	RecipeID    int     `json:"recipe_id"`
+	Recipe      Recipe  `json:"recipe"`
+}
